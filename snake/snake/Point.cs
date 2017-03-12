e3ce1cf6c47d04414b9e1583edf3973e9907f61b
AlexP79 <Alex.P79@mail..ru>
2017-03-12 12:36:42 +0800

    Изучили конструктор, инкапсуляцию

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -16,6 +16,13 @@ namespace Snake
         {
         }
 
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
